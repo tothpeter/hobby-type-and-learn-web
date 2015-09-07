937300535318a623811e5aaf546c63fc1c1b7735
Peter Toth <tothpeter08@gmail.com>
2015-09-08 00:01:25 +0200

    Render current user into html meta found by cookie value

--- Gemfile
+++ Gemfile
@@ -5,6 +5,13 @@ ruby '2.2.3'
 gem 'sinatra'
 gem 'redis'
 
+gem 'pg'
+gem 'activerecord'
+gem 'sinatra-activerecord'
+gem 'active_model_serializers', :git => 'git://github.com/rails-api/active_model_serializers.git', :ref => 'af280ab'
+
+gem 'sinatra-contrib'
+
 group :production do
   gem 'unicorn'
 end
--- app.rb
+++ app.rb
@@ -1,6 +1,34 @@
 require 'bundler'
 Bundler.require
 
+ActiveRecord::Base.establish_connection(
+  :adapter  => "postgresql",
+  # :host     => "localhost",
+  # :username => "",
+  # :password => "",
+  :database => "type_and_learn_api_development"
+)
+
+class User < ActiveRecord::Base
+  has_many :labels
+end
+
+class Label < ActiveRecord::Base
+  belongs_to :user
+end
+
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :email
+
+  has_many :labels
+end
+
+class LabelSerializer < ActiveModel::Serializer
+  attributes :title, :position, :user_id
+
+  belongs_to :user
+end
+
 get '*' do
   html = redis.get deploy_key
 
@@ -29,13 +57,23 @@ def redis
 end
 
 def insert_current_user_meta html
-  html.insert(head_pos(html), "<meta name='current-user' content='#{current_user_json}'>")
+  user = current_user
+
+  if user
+    serializer = UserSerializer.new user
+    adapter = ActiveModel::Serializer::Adapter::JsonApi.new serializer, include: ['labels']
+    current_user_json = adapter.serializable_hash
+
+    html.insert(head_pos(html), "<meta name='current-user' content='#{current_user_json}'>")
+  else
+    html
+  end
 end
 
 def head_pos html
   html.index(">", html.index("<head")) + 1
 end
 
-def current_user_json
-  "{a:1}"
+def current_user
+  user = User.find_by auth_token_for_web: cookies[:auth_token_for_web]
 end
