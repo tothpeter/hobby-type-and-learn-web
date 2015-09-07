40cf6103c082814662729354815d6aab0506cb6a
Peter Toth <tothpeter08@gmail.com>
2015-09-07 21:22:17 +0200

    Implement basic: reading index html content from redis

--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
+require 'redis'
 
 get '*' do
-  "Hello World!"
+  redis.get deploy_key
+end
+
+def deploy_key
+  params[:version] ||= redis.get "study-rack:current"
+  # params[:version] ||= 'release'
+  case params[:version]
+  when 'release' then 'release'
+  when 'canary'  then  redis.lindex('releases', 0)
+  else
+    params[:version]
+  end
+end
+
+def redis
+  # settings.development?
+  # settings.production?
+  if ENV['REDISTOGO_URL']
+    Redis.new(:url => ENV['REDISTOGO_URL'])
+  else
+    redis = Redis.new()
+  end
 end
